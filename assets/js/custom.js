2431fbbb87e24c895f715ee3bdc1f1d1d984c34f
Steve Polito <info@stevepolitodesign.com>
2019-04-20 13:50:39 -0400

    compile

---
+++ assets/js/custom.js
@@ -0,0 +1,74 @@
+"use strict";
+
+/**
+ * File customizer.js.
+ *
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+(function ($) {
+  // Site title and description.
+  wp.customize('blogname', function (value) {
+    value.bind(function (to) {
+      $('.site-title a').text(to);
+    });
+  });
+  wp.customize('blogdescription', function (value) {
+    value.bind(function (to) {
+      $('.site-description').text(to);
+    });
+  }); // Header text color.
+
+  wp.customize('header_textcolor', function (value) {
+    value.bind(function (to) {
+      if ('blank' === to) {
+        $('.site-title, .site-description').css({
+          'clip': 'rect(1px, 1px, 1px, 1px)',
+          'position': 'absolute'
+        });
+      } else {
+        $('.site-title, .site-description').css({
+          'clip': 'auto',
+          'position': 'relative'
+        });
+        $('.site-title a, .site-description').css({
+          'color': to
+        });
+      }
+    });
+  });
+})(jQuery);
+"use strict";
+
+/**
+ * File skip-link-focus-fix.js.
+ *
+ * Helps with accessibility for keyboard only users.
+ *
+ * Learn more: https://git.io/vWdr2
+ */
+(function () {
+  var isIe = /(trident|msie)/i.test(navigator.userAgent);
+
+  if (isIe && document.getElementById && window.addEventListener) {
+    window.addEventListener('hashchange', function () {
+      var id = location.hash.substring(1),
+          element;
+
+      if (!/^[A-z0-9_-]+$/.test(id)) {
+        return;
+      }
+
+      element = document.getElementById(id);
+
+      if (element) {
+        if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+          element.tabIndex = -1;
+        }
+
+        element.focus();
+      }
+    }, false);
+  }
+})();
