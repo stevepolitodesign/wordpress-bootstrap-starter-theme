5c75a67d890ae2d06a5b79e74e18d7cae034dddf
Steve Polito <info@stevepolitodesign.com>
2019-04-20 12:07:03 -0400

    remove scripts provided by _s and replace with concatenated script from wpgulp

--- functions.php
+++ functions.php
@@ -122,9 +122,7 @@ add_action( 'widgets_init', 'wordpress_bootstrap_starter_theme_widgets_init' );
 function wordpress_bootstrap_starter_theme_scripts() {
 	wp_enqueue_style( 'wordpress-bootstrap-starter-theme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'wordpress-bootstrap-starter-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'wordpress-bootstrap-starter-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'wordpress-bootstrap-starter-theme-custom-scripts', get_template_directory_uri() . '/assets/js/custom.min.js', array('customize-preview'), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
