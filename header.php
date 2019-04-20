120b20616e59d189339ecd33856a661b974b5a57
Steve Polito <info@stevepolitodesign.com>
2019-04-20 13:06:59 -0400

    use bootstrap screen reader class

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wordpress-bootstrap-starter-theme' ); ?></a>
+	<a class="sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'wordpress-bootstrap-starter-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
