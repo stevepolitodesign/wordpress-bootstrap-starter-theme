43f73b9a3d7a9bf5498e1e9f373d728390fe2e28
Steve Polito <info@stevepolitodesign.com>
2019-04-20 15:20:45 -0400

    update projectURL

--- wpgulp.config.js
+++ wpgulp.config.js
@@ -10,7 +10,7 @@
 module.exports = {
 
 	// Project options.
-	projectURL: 'wpgulp.local', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:3000 depending upon your local WordPress setup.
+	projectURL: 'wordpress-bootstrap-starter-theme.local', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:3000 depending upon your local WordPress setup.
 	productURL: './', // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
 	browserAutoOpen: false,
 	injectChanges: true,
