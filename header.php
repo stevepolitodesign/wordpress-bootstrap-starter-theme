9488e07273664ee5b1bf943699924b2a4df20b47
Steve Polito <info@stevepolitodesign.com>
2019-04-20 12:28:43 -0400

    remove navigation markup and script provided by _s

--- header.php
+++ header.php
@@ -45,7 +45,6 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wordpress-bootstrap-starter-theme' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
